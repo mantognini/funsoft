814ed35e8d5af6a0d4efe4ebfe6778273598df95
Frédéric Ouwehand <fred1270@gmail.com>
2014-09-25 11:18:32 +0200

    Improve smallStepRed tests

--- 01 arithmetic/1-arithmetic/src/test/scala/fos/SmallStepReductionTest.scala
+++ 01 arithmetic/1-arithmetic/src/test/scala/fos/SmallStepReductionTest.scala
@@ -26,7 +26,42 @@ class SmallStepReductionTest extends FlatSpec with Matchers {
   }
 
   "If statement" should "be properly reduced" in {
-    testReducedResult(If(True, True, True), "True")
+    testReducedResult(If(True, True, False), "True")
+    testReducedResult(If(False, True, False), "False")
+    testReducedResult(If(If(True, True, False), True, False), "True")
+    testReducedResult(If(If(False, True, False), True, False), "False")
+    testReducedResult(If(If(True, True, False), If(False, True, False), False), "False")
   }
 
+  "Pred" should "be properly reduced" in {
+    testReducedResult(Pred(Zero), "Zero")
+    testReducedResult(Pred(Succ(Zero)), "Zero")
+  }
+
+  "IsZero" should "be properly reduced" in {
+    testReducedResult(IsZero(Zero), "True")
+    testReducedResult(IsZero(Succ(Zero)), "False")
+    testReducedResult(IsZero(Succ(Pred(Zero))), "False")
+  }
+
+  "Congruences" should "be properly reduced" in {
+    testReducedResult(If(IsZero(Zero), True, False), "True")
+    testReducedResult(If(IsZero(Pred(Succ(Zero))), True, False), "True")
+    testReducedResult(If(IsZero(Succ(Pred(Zero))), True, False), "False")
+
+    testReducedResult(IsZero(Pred(Zero)), "True")
+    testReducedResult(IsZero(Pred(Succ(Zero))), "True")
+    testReducedResult(IsZero(Pred(Succ(Succ(Zero)))), "False")
+
+    testReducedResult(Pred(Pred(Succ(Succ(Succ(Zero))))), "Succ(Zero)")
+
+    testReducedResult(Succ(Pred(Pred(Succ(Zero)))), "Succ(Zero)")
+  }
+
+  "More complex compositions" should "be properly reduced" in {
+    testReducedResult(If(IsZero(Pred(Succ(Zero))), Succ(Pred(Zero)), Pred(Zero)), "Succ(Zero)")
+    testReducedResult(IsZero(If(If(IsZero(Succ(Zero)), True, False), True, Succ(Zero))), "False")
+  }
+
+  // TODO: Test failures
 }
