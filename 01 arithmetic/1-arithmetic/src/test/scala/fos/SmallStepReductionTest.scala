094cd07257ed431aaba87aaa2f8ed17ef571033d
Frédéric Ouwehand <fred1270@gmail.com>
2014-09-25 11:55:47 +0200

    Refactor way of testing for smallStepRed

--- 01 arithmetic/1-arithmetic/src/test/scala/fos/SmallStepReductionTest.scala
+++ 01 arithmetic/1-arithmetic/src/test/scala/fos/SmallStepReductionTest.scala
@@ -6,61 +6,69 @@ import java.io.ByteArrayOutputStream
 
 class SmallStepReductionTest extends FlatSpec with Matchers {
 
-  def testReducedResult(input: Term, finalResult: String) {
+  def testRed(input: Term)(assertFun: String => Unit): Unit = {
     val output = new ByteArrayOutputStream
     Console.withOut(output) {
       Arithmetic.smallStepRed(input)
     }
-    """(.*)$""".r findFirstIn output.toString match {
-      case Some(lastLine) => lastLine shouldBe (finalResult)
+    assertFun(output.toString)
+  }
+
+  def lastLine(expected: String)(output: String): Unit = {
+    """(.*)$""".r findFirstIn output match {
+      case Some(lastLine) => lastLine shouldBe (expected)
       case e => throw new Exception("smallStepRed returned <<" + output.toString + ">> and it seams like empty string")
     }
   }
 
   "Values" should "be left as it" in {
-    testReducedResult(False, "False")
-    testReducedResult(True, "True")
-    testReducedResult(Zero, "Zero")
-    testReducedResult(Succ(Zero), "Succ(Zero)")
-    testReducedResult(Succ(Succ(Zero)), "Succ(Succ(Zero))")
+    testRed(False)(lastLine("False"))
+    testRed(True)(lastLine("True"))
+    testRed(Zero)(lastLine("Zero"))
+    testRed(Succ(Zero))(lastLine("Succ(Zero)"))
+    testRed(Succ(Succ(Zero)))(lastLine("Succ(Succ(Zero))"))
   }
 
   "If statement" should "be properly reduced" in {
-    testReducedResult(If(True, True, False), "True")
-    testReducedResult(If(False, True, False), "False")
-    testReducedResult(If(If(True, True, False), True, False), "True")
-    testReducedResult(If(If(False, True, False), True, False), "False")
-    testReducedResult(If(If(True, True, False), If(False, True, False), False), "False")
+    testRed(If(True, True, False))(lastLine("True"))
+    testRed(If(False, True, False))(lastLine("False"))
+    testRed(If(If(True, True, False), True, False))(lastLine("True"))
+    testRed(If(If(False, True, False), True, False))(lastLine("False"))
+    testRed(If(If(True, True, False), If(False, True, False), False))(lastLine("False"))
   }
 
   "Pred" should "be properly reduced" in {
-    testReducedResult(Pred(Zero), "Zero")
-    testReducedResult(Pred(Succ(Zero)), "Zero")
+    testRed(Pred(Zero))(lastLine("Zero"))
+    testRed(Pred(Succ(Zero)))(lastLine("Zero"))
   }
 
   "IsZero" should "be properly reduced" in {
-    testReducedResult(IsZero(Zero), "True")
-    testReducedResult(IsZero(Succ(Zero)), "False")
-    testReducedResult(IsZero(Succ(Pred(Zero))), "False")
+    testRed(IsZero(Zero))(lastLine("True"))
+    testRed(IsZero(Succ(Zero)))(lastLine("False"))
+    testRed(IsZero(Succ(Pred(Zero))))(lastLine("False"))
   }
 
   "Congruences" should "be properly reduced" in {
-    testReducedResult(If(IsZero(Zero), True, False), "True")
-    testReducedResult(If(IsZero(Pred(Succ(Zero))), True, False), "True")
-    testReducedResult(If(IsZero(Succ(Pred(Zero))), True, False), "False")
+    testRed(If(IsZero(Zero), True, False))(lastLine("True"))
+    testRed(If(IsZero(Pred(Succ(Zero))), True, False))(lastLine("True"))
+    testRed(If(IsZero(Succ(Pred(Zero))), True, False))(lastLine("False"))
 
-    testReducedResult(IsZero(Pred(Zero)), "True")
-    testReducedResult(IsZero(Pred(Succ(Zero))), "True")
-    testReducedResult(IsZero(Pred(Succ(Succ(Zero)))), "False")
+    testRed(IsZero(Pred(Zero)))(lastLine("True"))
+    testRed(IsZero(Pred(Succ(Zero))))(lastLine("True"))
+    testRed(IsZero(Pred(Succ(Succ(Zero)))))(lastLine("False"))
 
-    testReducedResult(Pred(Pred(Succ(Succ(Succ(Zero))))), "Succ(Zero)")
+    testRed(Pred(Pred(Succ(Succ(Succ(Zero))))))(lastLine("Succ(Zero)"))
 
-    testReducedResult(Succ(Pred(Pred(Succ(Zero)))), "Succ(Zero)")
+    testRed(Succ(Pred(Pred(Succ(Zero)))))(lastLine("Succ(Zero)"))
   }
 
   "More complex compositions" should "be properly reduced" in {
-    testReducedResult(If(IsZero(Pred(Succ(Zero))), Succ(Pred(Zero)), Pred(Zero)), "Succ(Zero)")
-    testReducedResult(IsZero(If(If(IsZero(Succ(Zero)), True, False), True, Succ(Zero))), "False")
+    testRed(If(IsZero(Pred(Succ(Zero))), Succ(Pred(Zero)), Pred(Zero)))(lastLine("Succ(Zero)"))
+    testRed(IsZero(If(If(IsZero(Succ(Zero)), True, False), True, Succ(Zero))))(lastLine("False"))
+  }
+
+  "unreduceable inputs" should "produce an stuck term message" in {
+
   }
 
   // TODO: Test failures
