5ccd8c6bed50c1f3f181e346afbccea892db359d
Frédéric Ouwehand <fred1270@gmail.com>
2014-09-27 10:09:55 +0200

    Refactor: get testing values

--- 01 arithmetic/1-arithmetic/src/test/scala/fos/SmallStepReductionTest.scala
+++ 01 arithmetic/1-arithmetic/src/test/scala/fos/SmallStepReductionTest.scala
@@ -27,23 +27,17 @@ class SmallStepReductionTest extends FlatSpec with Matchers {
       case e => reductionOutput should contain("a well-formed stuck term output")
     }
   }
-  
+
   def matchExactly(expected: String)(reductionOutput: String): Unit = {
     reductionOutput shouldBe expected
   }
-  
+
   def testLL = ttools.testReduction(testRed, lastLine)_
   def testST = ttools.testReduction(testRed, stuckTerm)_
   def testME = ttools.testReduction(testRed, matchExactly)_
 
   "Values" should "be left as it" in {
-    testLL(False, "False")
-    
-    testLL(False, "False")
-    testLL(True, "True")
-    testLL(Zero, "Zero")
-    testLL(Succ(Zero), "Succ(Zero)")
-    testLL(Succ(Succ(Zero)), "Succ(Succ(Zero))")
+    ttools.getSomeEvaluationTestingValues()("values") foreach (x => testLL(x._1, x._2))
   }
 
   "If statement" should "be properly reduced" in {
@@ -101,25 +95,23 @@ class SmallStepReductionTest extends FlatSpec with Matchers {
     testME(Zero, "Zero\n")
     testME(Succ(Zero), "Succ(Zero)\n")
     testME(Succ(Succ(Zero)), "Succ(Succ(Zero))\n")
-    
+
     // 1 or more evaluation steps
     testME(IsZero(Zero), "IsZero(Zero)\nTrue\n")
-    testME(If(IsZero(Succ(Zero)), True, Pred(Zero)), 
-"""If(IsZero(Succ(Zero)),True,Pred(Zero))
+    testME(If(IsZero(Succ(Zero)), True, Pred(Zero)),
+      """If(IsZero(Succ(Zero)),True,Pred(Zero))
 If(False,True,Pred(Zero))
 Pred(Zero)
 Zero
-"""
-    )
-    testME(If(IsZero(Pred(Pred(Succ(Succ(Zero))))),If(IsZero(Zero),True,False),False), 
-"""If(IsZero(Pred(Pred(Succ(Succ(Zero))))),If(IsZero(Zero),True,False),False)
+""")
+    testME(If(IsZero(Pred(Pred(Succ(Succ(Zero))))), If(IsZero(Zero), True, False), False),
+      """If(IsZero(Pred(Pred(Succ(Succ(Zero))))),If(IsZero(Zero),True,False),False)
 If(IsZero(Pred(Succ(Zero))),If(IsZero(Zero),True,False),False)
 If(IsZero(Zero),If(IsZero(Zero),True,False),False)
 If(True,If(IsZero(Zero),True,False),False)
 If(IsZero(Zero),True,False)
 If(True,True,False)
 True
-"""
-    )
+""")
   }
 }
--- 01 arithmetic/1-arithmetic/src/test/scala/fos/ttools.scala
+++ 01 arithmetic/1-arithmetic/src/test/scala/fos/ttools.scala
@@ -1,10 +1,22 @@
 package fos
 
+import org.scalatest.Exceptional
+import scala.collection.immutable.List
+import scala.collection.immutable.Map
+
 object ttools {
-  
-	def testReduction(redFun: Term => (String => Unit) => Unit, testFun: String => String => Unit)
-	(t: Term, expected: String): Unit = {
-	  redFun(t)(testFun(expected))
-	}
-	
+
+  def testReduction(redFun: Term => (String => Unit) => Unit, testFun: String => String => Unit)(t: Term, expected: String): Unit = {
+    redFun(t)(testFun(expected))
+  }
+
+  def getSomeEvaluationTestingValues(): Map[String, List[Tuple2[Term, String]]] = {
+    Map(
+      "values" -> List(
+        (False, "False"),
+        (True, "True"),
+        (Succ(Zero), "Succ(Zero)"),
+        (Succ(Succ(Zero)), "Succ(Succ(Zero))")))
+  }
+
 }
