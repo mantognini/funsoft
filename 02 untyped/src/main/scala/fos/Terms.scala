ec7be1343f86bdb24bd7cefed97f56f6f28f7427
Marco Antognini <antognini.marco@gmail.com>
2014-10-03 17:32:41 +0200

    add some terms

--- 02 untyped/src/main/scala/fos/Terms.scala
+++ 02 untyped/src/main/scala/fos/Terms.scala
@@ -5,4 +5,7 @@ import scala.util.parsing.input.Positional
 /** Abstract Syntax Trees for terms. */
 abstract class Term extends Positional
 
-//   ... To complete ... 
+case class Var(name: String) extends Term // x 
+case class Abs(arg: Var, body: Term) extends Term // \x.t
+case class App(left: Term, right: Term) extends Term // t t
+case class Par(t: Term) extends Term // ( t )
