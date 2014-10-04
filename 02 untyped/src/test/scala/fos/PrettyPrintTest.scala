e1e6a89829f30f921335f8eb1e59e28f275d3884
Marco Antognini <antognini.marco@gmail.com>
2014-10-04 20:13:20 +0200

    add more tests

--- 02 untyped/src/test/scala/fos/PrettyPrintTest.scala
+++ 02 untyped/src/test/scala/fos/PrettyPrintTest.scala
@@ -19,6 +19,9 @@ class PrettyPrintTest extends FlatSpec with Matchers {
     App(App(Abs(x, Abs(f, x)), y), z) -> """(\x. \f. x) y z""",
     App(x, Abs(y, y)) -> """x (\y. y)""",
     App(Abs(x, x), Abs(y, y)) -> """(\x. x) (\y. y)""",
+    App(App(z, Abs(x, x)), Abs(y, y)) -> """z (\x. x) (\y. y)""",
+    App(App(Abs(z, z), Abs(x, x)), Abs(y, y)) -> """(\z. z) (\x. x) (\y. y)""",
+    App(Abs(g, App(g, Abs(x, x))), Abs(f, f)) -> """(\g. g \x. x) (\f. f)""",
     App(App(Abs(x, Abs(y, App(x, y))), f), g) -> """(\x. \y. x y) f g""",
     App(App(App(Abs(x, Abs(y, App(x, y))), Abs(z, z)), f), g) -> """(\x. \y. x y) (\z. z) f g""",
     App(Abs(x, Abs(y, App(App(z, x), y))), Abs(x, x)) -> """(\x. \y. (z x) y) (\x. x)""")
