96d65b3d479624727cb161ecd1adb20b4c7c6bbf
Marco Antognini <antognini.marco@gmail.com>
2014-10-04 14:59:06 +0200

    Revert "remove Par term (useless)"
    This reverts commit d7d9f973650429f45f3a9c515f320269ff2d86c5.

--- 02 untyped/src/main/scala/fos/Terms.scala
+++ 02 untyped/src/main/scala/fos/Terms.scala
@@ -8,3 +8,4 @@ abstract class Term extends Positional
 case class Var(name: String) extends Term // x 
 case class Abs(arg: Var, body: Term) extends Term // \x.t
 case class App(left: Term, right: Term) extends Term // t t
+case class Par(t: Term) extends Term // ( t )
