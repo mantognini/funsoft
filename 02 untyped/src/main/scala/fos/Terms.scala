f2b37fc8eaf791d2f7f86930ab71891f5c483644
Marco Antognini <antognini.marco@gmail.com>
2014-10-04 19:01:16 +0200

    add a first version of the pretty printer (buggy)

--- 02 untyped/src/main/scala/fos/Terms.scala
+++ 02 untyped/src/main/scala/fos/Terms.scala
@@ -5,6 +5,16 @@ import scala.util.parsing.input.Positional
 /** Abstract Syntax Trees for terms. */
 abstract class Term extends Positional
 
-case class Var(name: String) extends Term // x 
+case class Var(name: String) extends Term // x
+{ override def toString = name }
+
 case class Abs(arg: Var, body: Term) extends Term // \x.t
+{ override def toString = "\\" + arg + ". " + body }
+
 case class App(left: Term, right: Term) extends Term // t t
+{
+  override def toString = left match {
+    case App(_, _) => "(" + left + ") " + right
+    case _ => left + " " + right
+  }
+}
