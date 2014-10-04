5198122566cc09b8d89d439e301137775a1c3bbf
Marco Antognini <antognini.marco@gmail.com>
2014-10-04 19:29:42 +0200

    improve pretty print

--- 02 untyped/src/main/scala/fos/Terms.scala
+++ 02 untyped/src/main/scala/fos/Terms.scala
@@ -13,8 +13,24 @@ case class Abs(arg: Var, body: Term) extends Term // \x.t
 
 case class App(left: Term, right: Term) extends Term // t t
 {
-  override def toString = left match {
-    case App(_, _) => "(" + left + ") " + right
-    case _ => left + " " + right
+  override def toString = {
+    def withPar(t: Term) = t match {
+      case Var(_) => t // ignore parentheses for simple var
+      case _ => "(" + t + ")"
+    }
+
+    def withLeftPar = withPar(left) + " " + right
+    def withRightPar = left + " " + withPar(right)
+    def withLeftRightPar = withPar(left) + " " + withPar(right)
+    def withoutPar = left + " " + right
+
+    (left, right) match {
+      case (Abs(_, _), Abs(_, _)) => withLeftRightPar
+      case (Abs(_, _), _) => withLeftPar
+      case (_, Abs(_, _)) => withRightPar
+      case (App(Abs(_, _), _), _) => withoutPar
+      case (App(_, _), _) => withLeftPar
+      case _ => withoutPar
+    }
   }
 }
