f41760cf3111d6429d6aa5533d0938a7f38d9786
Marco Antognini <antognini.marco@gmail.com>
2014-10-09 10:48:15 +0200

    add some basic input -> parse -> print tests

--- 02 untyped/src/test/scala/fos/ParserPrinterTest.scala
+++ 02 untyped/src/test/scala/fos/ParserPrinterTest.scala
@@ -12,7 +12,21 @@ class ParserPrinterTest extends FlatSpec with Matchers {
 
   // Test input => parse => print
   val tests = Map[String, String](
-    """x""" -> """x""")
+    // WITHOUT any variable name substitution
+    // Base case
+    """x""" -> """x""",
+    """x y""" -> """x y""",
+    """\x. x""" -> """\x. x""",
+    """y \x. x""" -> """y \x. x""",
+    """\x. x y""" -> """\x. x y""",
+    """z \x. x y""" -> """z \x. x y""",
+    """(\x. x) y""" -> """(\x. x) y""",
+    // Remove extra parentheses
+    """(x)""" -> """x""",
+    """((x y))""" -> """x y""",
+    """((\x. (x))) ((y z))""" -> """(\x. x) (y z)""")
+
+  // TODO add more tests
 
   tests.foreach {
     case (in, out) => it should "properly parse <" + in + "> and print <" + out + ">" in {
