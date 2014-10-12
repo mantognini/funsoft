3f94c1409765542313d7da9a4c7e788af608f964
Frédéric Ouwehand <fred1270@gmail.com>
2014-10-12 21:55:45 +0200

    Add more failing test #3

--- 02 untyped/src/test/scala/fos/ttools.scala
+++ 02 untyped/src/test/scala/fos/ttools.scala
@@ -22,8 +22,16 @@ object ttools {
     """x y z f""" -> App(App(App(x, y), z), f),
     """x y z f g""" -> App(App(App(App(x, y), z), f), g),
 
+    """(x y)(f g)""" -> App(App(x, y), App(f, g)),
+    """(x y z)(f g x)""" -> App(App(App(x, y), z), App(App(f, g), x)),
+    """(x y z f)(g x)""" -> App(App(App(App(x, y), z), f), App(g, x)),
+    """(x x x x)(y y y y)""" -> App(App(App(App(App(x, x), x), x), x),
+      App(App(App(App(y, y), y), y), y)),
+
     """\y. y""" -> Abs(y, y),
     """\x. x y z""" -> Abs(x, App(App(x, y), z)),
+    """\x. x y z f""" -> Abs(x, App(App(App(x, y), z), f)),
+    """\x. x y z f g""" -> Abs(x, App(App(App(App(x, y), z), f), g)),
 
     """\y. \x. x y""" -> Abs(y, Abs(x, App(x, y))),
     """\x. \y. x y x""" -> Abs(x, Abs(y, (App(App(x, y), x)))), // λx. (λy. ((x y) x))
@@ -37,8 +45,10 @@ object ttools {
     """(\x. x) (y z)""" -> App(Abs(x, x), App(y, z)),
 
     """(\x. \y. x y) (\z. z) f g""" -> App(App(App(Abs(x, Abs(y, App(x, y))), Abs(z, z)), f), g),
-    """(\f. f f g z) \x. g g g x""" -> App(Abs(f, App(f, App(f, App(g, z)))), Abs(x, App(g, App(g, App(g, x))))))
-
+    """(\f. f f g z) \x. g g g x""" -> App(Abs(f, App(f, App(f, App(g, z)))),
+      Abs(x, App(g, App(g, App(g, x))))),
+    """(\f. f f g z x) \x. g g g x x""" -> App(Abs(f, App(App(f, App(f, App(g, z))), x)),
+      Abs(x, App(App(g, App(g, App(g, x))), x))))
   // These string are given with extra ()'s, but still well formated
   val wellFormatedCases = Map[String, Term](
     """\x. (x y z)""" -> Abs(x, App(App(x, y), z)),
@@ -53,7 +63,10 @@ object ttools {
     """\f. f (f succ0)""" -> Abs(f, App(f, App(f, Var("succ0")))),
     """\f. f (f (succ0))""" -> Abs(f, App(f, App(f, Var("succ0")))),
 
-    """(\f. f (f (g z))) (\x. g (g (g x)))""" -> App(Abs(f, App(f, App(f, App(g, z)))), Abs(x, App(g, App(g, App(g, x))))))
+    """(\f. f (f (g z))) (\x. g (g (g x)))""" -> App(Abs(f, App(f, App(f, App(g, z)))), Abs(x, App(g, App(g, App(g, x))))),
+
+    """(\f. f f g z) (\x. g g g x)""" -> App(Abs(f, App(f, App(f, App(g, z)))),
+      Abs(x, App(g, App(g, App(g, x))))))
 
   val correctCases = canonicalCases ++ wellFormatedCases
 
