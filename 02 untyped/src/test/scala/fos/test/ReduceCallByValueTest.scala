713e56f6204cca45d14065bfd050764b35a3d4a9
Marco Antognini <antognini.marco@gmail.com>
2014-10-13 22:38:11 +0200

    some tests rcbv test are wrong - removing everything and re-adding only when sure
    also, add some 🍻 to the log to get some motivation

--- 02 untyped/src/test/scala/fos/test/ReduceCallByValueTest.scala
+++ 02 untyped/src/test/scala/fos/test/ReduceCallByValueTest.scala
@@ -28,130 +28,24 @@ class ReduceCallByValueTest extends FlatSpec with Matchers with GivenWhenThen {
   // Note: 	We assume that the parser and prettyPrinter are correct
   // 		and we write the terms with strings for readability
   val cbvrCasesWhichTerminate: List[List[String]] = List(
-    // Examples seen somewhere
-    List(
-      """\y.(\x.x) y""", // Statement example
-      """\y.y"""),
-    List(
-      """(\x.x) ((\x.x) (\z.(\x.x) z))""", // from TAPL p.56
-      """(\x.x) \z.(\x.x) z""",
-      """\z.(\x.x) z""",
-      """\z.z"""),
+    /// Normal forms
+    """\x. x""" :: Nil,
+    """\x. \y. y""" :: Nil,
+    """\x. \y. x y""" :: Nil,
+    """x""" :: Nil,
+    """x y""" :: Nil,
+    """x y z""" :: Nil,
+    """x y z a""" :: Nil,
+    """x y z a b""" :: Nil,
+    """x x x x x y y y z""" :: Nil,
+    """(\x. x) y""" :: Nil, // yes, y is NOT a value.
+    // Statement example
+    """\y. (\x. x) y""" :: Nil,
 
-    // without substitutions - base cases
-    List(
-      """\x.x"""),
-    List(
-      """\x.\y.y"""),
-    List(
-      """\x.\y.x y"""),
-    List(
-      """(\x.x) y""",
-      """y"""),
-    List(
-      """(\x.a b) y""",
-      """a b"""),
-    List(
-      """(\x.x x) y""",
-      """y y"""),
-    List(
-      """x y z"""),
-    List(
-      """x (\y.y) z"""), // yes, z is applied to (x (\y.y)) and not only (\y.y) -> we are stuck there
-    List(
-      """x (\y.y) \z.z"""), // same there
-    List(
-      """(\x.x x) \y.y""",
-      """(\y.y) \y.y""",
-      """\y.y"""),
-    List(
-      """(\x.x z) \y.y""",
-      """(\y.y) z""",
-      """z"""),
-    List(
-      """(\x.z x) \y.y""",
-      """z \y.y"""),
-    List(
-      """(\x.z x) \y.y \f.f""",
-      """z \y.y \f.f"""),
-    List(
-      """(\x.x z g) \y.y \f.f""",
-      """(\y.y \f.f) z g""",
-      """z (\f.f) g"""),
-    List(
-      """(\x.x) g ((\y.y) f)""", // Priority of evaluations rules for normal-reduction
-      """g ((\y.y) f)""",
-      """g f"""),
-    List(
-      """(\x.x) (\z.z) ((\y.y) f)""",
-      """(\z.z) ((\y.y) f)""",
-      """(\y.y) f""",
-      """f"""),
+    /// Non-normal forms
+    // from TAPL p.57
+    """(\x. x) ((\x. x) (\z. (\x. x) z))""" :: """(\x. x) (\z. (\x. x) z)""" :: """\z. (\x. x) z""" :: Nil,
 
-    // without substitutions - trickier cases
-    List(
-      """(\x.x) (\y.y) \z.z""",
-      """(\y.y) \z.z""",
-      """\z.z"""),
-    List(
-      """(\x.(\f. f) x) (\y.y) \z.z""",
-      """(\x.x) (\y.y) \z.z""",
-      """(\y.y) \z.z""",
-      """\z.z"""),
-    List(
-      """(\x.\f.f) (\y.y) \z.z""",
-      """(\f.f) \z.z""",
-      """\z.z"""),
-    List(
-      """(\x.\f.x) (\y.y) \z.z""",
-      """(\f.\y.y) \z.z""",
-      """\y.y"""),
-    List(
-      """(\x.x \y.y ((\f.g) \z.z)) (\f.f) g""",
-      """(\x.x \y.y g) (\f.f) g""",
-      """((\f.f) \y.y g) g""",
-      """(\y.y g) g""",
-      """g g"""), // pam-padam-pa
-    List(
-      """(\x.x \y.y ((\f.g) \z.z)) ((\f.f) g)""",
-      """(\x.x \y.y g) ((\f.f) g)""",
-      """(\f.f) g \y.y g""",
-      """g \y.y g"""),
-    List(
-      """(\x.x \y.y ((\f.g) \z.z)) ((\f.f) g) (\x.x x) y""",
-      """(\x.x \y.y g) ((\f.f) g) (\x.x x) y""",
-      """((\f.f) g \y.y g) (\x.x x) y""",
-      """g (\y.y g) (\x.x x) y""" // We are stuck, because y is applied to "g (\y.y g) (\x.x x)"
-      // and not only to (\x.x x)
-      ),
-    List(
-      """(\x.x \y.y ((\f.g) \z.z)) ((\f.f) g) ((\x.x) x) y""",
-      """(\x.x \y.y g) ((\f.f) g) ((\x.x) x) y""",
-      """((\f.f) g \y.y g) ((\x.x) x) y""",
-      """g (\y.y g) ((\x.x) x) y""", // This time, we can do one more red. since there is still a redex
-      """g (\y.y g) x y"""),
-    // TODO: more tests as the one above are welcome, since these kind of cases, were normal-red won't
-    // stuck and other reduction strategies will, distinguish them
-
-    List(
-      """(\x.x \y.y ((\f.f) \g.g)) ((\f.f) \y.y) (\x.x x) y""",
-      """(\x.x \y.y \g.g) ((\f.f) \y.y) (\x.x x) y""",
-      """(\f.f) (\y.y) (\y.y \g.g) (\x.x x) y""",
-      """(\y.y) (\y.y \g.g) (\x.x x) y""",
-      """(\y.y \g.g) (\x.x x) y""",
-      """(\x.x x) (\g.g) y""",
-      """(\g.g) (\g.g)  y""",
-      """(\g.g)  y""",
-      """y"""),
-    List(
-      """(\x.x \y.y ((\f.f) \g.g)) ((\f.f) \y.y) ((\x.x) x) y""",
-      """(\x.x \y.y \g.g) ((\f.f) \y.y) ((\x.x) x) y""",
-      """(\f.f) (\y.y) (\y.y \g.g) ((\x.x) x) y""",
-      """(\y.y) (\y.y \g.g) ((\x.x) x) y""",
-      """(\y.y \g.g) ((\x.x) x) y""",
-      """((\x.x) x) (\g.g) y""",
-      """x (\g.g) y"""),
-    List(),
     List())
 
   def parse(input: String) = Untyped.parseOrDie(input)
@@ -165,6 +59,8 @@ class ReduceCallByValueTest extends FlatSpec with Matchers with GivenWhenThen {
       val output = reduceOnce(parse(input))
       info("an exception was expected - instead the input was reduced to " + output)
     }
+
+    info("🍻")
   }
 
   def testSteps(steps: List[String]) {
@@ -180,6 +76,8 @@ class ReduceCallByValueTest extends FlatSpec with Matchers with GivenWhenThen {
       val r = parse(reduced).toString
 
       i shouldBe r
+
+      info("🍺")
 
       initial // is the reduced of the next test
     }
