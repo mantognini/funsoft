f6d6a2d6b4dd25a965360f1ee7c55e1378b142b0
Marco Antognini <antognini.marco@gmail.com>
2014-10-16 16:31:00 +0200

    add terms and types

--- 03 typed assignment/src/main/scala/fos/Terms.scala
+++ 03 typed assignment/src/main/scala/fos/Terms.scala
@@ -3,23 +3,78 @@ package fos
 import scala.util.parsing.input.Positional
 
 /** Abstract Syntax Trees for terms. */
-abstract class Term extends Positional
+abstract class Term extends Positional {
+  def prettyString( /* TODO ARG */ ): String
+  final override def toString = prettyString
+}
 
 case object True extends Term {
-  override def toString() = "true"
+  override def prettyString = "true"
 }
 
 case object False extends Term {
-  override def toString() = "false"
+  override def prettyString = "false"
 }
 case object Zero extends Term {
-  override def toString() = "0"
+  override def prettyString = "0"
 }
-  //   ... To complete ... 
+
+case class Var(name: String) extends Term {
+  override def prettyString = ???
+}
+
+case class Abs(x: Var, typ: Type, body: Term) extends Term {
+  override def prettyString = ???
+}
+
+case class App(t1: Term, t2: Term) extends Term {
+  override def prettyString = ???
+}
+
+case class Succ(t: Term) extends Term {
+  override def prettyString = ???
+}
+
+case class Pred(t: Term) extends Term {
+  override def prettyString = ???
+}
+
+case class IsZero(t: Term) extends Term {
+  override def prettyString = ???
+}
+
+case class If(cond: Term, zen: Term, elze: Term) extends Term {
+  override def prettyString = ???
+}
+
+case class Pair(fst: Term, snd: Term) extends Term {
+  override def prettyString = ???
+}
+
+case class First(p: Term) extends Term {
+  override def prettyString = ???
+}
+
+case class Second(p: Term) extends Term {
+  override def prettyString = ???
+}
+
 /** Abstract Syntax Trees for types. */
 abstract class Type extends Term
 
-case object TypeBool extends Type {
-  override def toString() = "Bool"
+case object Bool extends Type {
+  override def prettyString = "Bool"
 }
-  //   ... To complete ... 
+
+case object Nat extends Type {
+  override def prettyString = "Nat"
+}
+
+case class Function(i: Type, o: Type) extends Type {
+  override def prettyString = ???
+}
+
+case class Product(fst: Type, snd: Type) extends Type {
+  override def prettyString = ???
+}
+
