b8518631cf3ef0d364c06f1f7761da80dace2b44
Frédéric Ouwehand <fred1270@gmail.com>
2014-10-16 09:32:49 +0200

    Sketch SimpleTerm parser

--- 03 typed assignment/src/main/scala/fos/SimplyTyped.scala
+++ 03 typed assignment/src/main/scala/fos/SimplyTyped.scala
@@ -3,22 +3,28 @@ package fos
 import scala.util.parsing.combinator.syntactical.StandardTokenParsers
 import scala.util.parsing.input._
 
-/** This object implements a parser and evaluator for the
+/**
+ * This object implements a parser and evaluator for the
  *  simply typed lambda calculus found in Chapter 9 of
  *  the TAPL book.
  */
 object SimplyTyped extends StandardTokenParsers {
   lexical.delimiters ++= List("(", ")", "\\", ".", ":", "=", "->", "{", "}", ",", "*")
-  lexical.reserved   ++= List("Bool", "Nat", "true", "false", "if", "then", "else", "succ",
-                              "pred", "iszero", "let", "in", "fst", "snd")
+  lexical.reserved ++= List("Bool", "Nat", "true", "false", "if", "then", "else", "succ",
+    "pred", "iszero", "let", "in", "fst", "snd")
 
-  /** Term     ::= SimpleTerm { SimpleTerm }
+  def convertNumeric(x: Int): Term = if (x <= 0) Zero else Succ(convertNumeric(x - 1))
+  def convertLet(x: String, T: Type, t1: Term, t2: Term) = App(Abs(Var(x), t2), t1) // TODO: What to do with T ?
+
+  /**
+   * Term     ::= SimpleTerm { SimpleTerm }
    */
   def Term: Parser[Term] = positioned(
-  //   ... To complete ... 
-    | failure("illegal start of term"))
+    //   ... To complete ... 
+    | failure ("illegal start of term"))
 
-  /** SimpleTerm ::= "true"
+  /**
+   * SimpleTerm ::= "true"
    *               | "false"
    *               | number
    *               | "succ" Term
@@ -34,16 +40,31 @@ object SimplyTyped extends StandardTokenParsers {
    *               | "snd" Term
    */
   def SimpleTerm: Parser[Term] = positioned(
-      "true"          ^^^ True
-    | "false"         ^^^ False
-  //   ... To complete ... 
-    | failure("illegal start of simple term"))
+    "true" ^^^ True
+      | "false" ^^^ False
+      | numericLit ^^ { case chars => convertNumeric(chars.toInt) }
+      | "succ" ~> Term ^^ { case e => Succ(e) }
+      | "pred" ~> Term ^^ { case e => Pred(e) }
+      | "iszero" ~> Term ^^ { case e => IsZero(e) }
+      | ("if" ~> Term) ~ ("then" ~> Term) ~ ("else" ~> Term) ^^ { case cond ~ zen ~ elze => If(cond, zen, elze) }
+      | ident ^^ { Var(_) }
+      | "\\" ~> ident ~ (":" ~> Type) ~ ("." ~> Term) ^^ { case name ~ T ~ term => Abs(Var(name), term) /* TODO: What to do with T? */ }
+      | "(" ~> Term <~ ")"
+      | ("let" ~> ident) ~ (":" ~> Type) ~ ("=" ~> Term) ~ ("in" ~> Term) ^^ {
+        // let x: T = t1 in t2		=>		(\x:T.t2) t1
+        case x ~ T ~ t1 ~ t2 => convertLet(x, T, t1, t2) // TODO
+      }
+      | ("{" ~> Term <~ ",") ~ (Term <~ "}") ^^ { case p1 ~ p2 => Pairr(p1, p2) }
+      | "fst" ~> Term ^^ { case p => Fst(p) }
+      | "snd" ~> Term ^^ { case p => snd(p) }
+      | failure("illegal start of simple term"))
 
-  /** Type       ::= SimpleType [ "->" Type ]
+  /**
+   * Type       ::= SimpleType [ "->" Type ]
    */
   def Type: Parser[Type] = positioned(
-  //   ... To complete ... 
-    | failure("illegal start of type"))
+    //   ... To complete ... 
+    | failure ("illegal start of type"))
 
   //   ... To complete ... 
 
@@ -61,24 +82,25 @@ object SimplyTyped extends StandardTokenParsers {
 
   /** Is the given term a numeric value? */
   def isNumericVal(t: Term): Boolean = t match {
-  //   ... To complete ... 
+    //   ... To complete ... 
     case _ => false
   }
 
   /** Is the given term a value? */
   def isValue(t: Term): Boolean = t match {
-  //   ... To complete ... 
+    //   ... To complete ... 
     case _ => false
   }
 
   /** Call by value reducer. */
   def reduce(t: Term): Term = t match {
-  //   ... To complete ... 
+    //   ... To complete ... 
     case _ =>
       throw NoRuleApplies(t)
   }
 
-  /** Returns the type of the given term <code>t</code>.
+  /**
+   * Returns the type of the given term <code>t</code>.
    *
    *  @param ctx the initial context
    *  @param t   the given term
@@ -87,10 +109,11 @@ object SimplyTyped extends StandardTokenParsers {
   def typeof(ctx: Context, t: Term): Type = t match {
     case True | False =>
       TypeBool
-  //   ... To complete ... 
+    //   ... To complete ... 
   }
 
-  /** Returns a stream of terms, each being one step of reduction.
+  /**
+   * Returns a stream of terms, each being one step of reduction.
    *
    *  @param t      the initial term
    *  @param reduce the evaluation strategy used for reduction.
