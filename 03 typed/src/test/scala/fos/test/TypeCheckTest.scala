3292df32c96d500fb8495c54cd971f8a855792dc
Marco Antognini <antognini.marco@gmail.com>
2014-10-25 11:50:32 +0200

    add positive tests for fig. 3

--- 03 typed/src/test/scala/fos/test/TypeCheckTest.scala
+++ 03 typed/src/test/scala/fos/test/TypeCheckTest.scala
@@ -17,8 +17,20 @@ class TypeCheckTest extends WordSpec with Matchers {
 
   // List of test that should pass
   val positives =
-    "0" -> Nat ::
+    "true" -> Bool ::
+      "false" -> Bool ::
+      "0" -> Nat ::
+      "pred 0" -> Nat ::
       "1" -> Nat ::
+      "pred 1" -> Nat ::
+      "iszero 0" -> Bool ::
+      "iszero 1" -> Bool ::
+      "if true then 0 else 1" -> Nat ::
+      """\x: Bool. true""" -> Function(Bool, Bool) ::
+      """\x: Nat. true""" -> Function(Nat, Bool) ::
+      """\x: Bool. x""" -> Function(Bool, Bool) :: // This tests x:T ∈ Γ
+      """(\x: Bool. x) true""" -> Bool ::
+      """(\x: Bool -> Bool. x true) (\x: Bool. x)""" -> Bool ::
       Nil
 
   def testNegative(input: String) {
@@ -32,7 +44,7 @@ class TypeCheckTest extends WordSpec with Matchers {
 
   def testPositive(test: (String, Type)) {
     val (input, expectedType) = test
-    s"deduce the type of $input to be expectedType" in {
+    s"deduce the type of $input to be $expectedType" in {
       val tree = parseOrFail(input)
       tryOrFail[TypeError] {
         val typ = SimplyTyped.typeof(tree)
