83190eca7c7135f4d0f5ca70cdefb7ef065c51bc
Frédéric Ouwehand <fred1270@gmail.com>
2014-11-01 12:57:55 +0100

    test skeleton

---
+++ 04 with extensions/src/test/scala/fos/test/SumTypesTest.scala
@@ -0,0 +1,10 @@
+package fos.test
+
+import org.scalatest._
+
+class SumTypesTest extends WordSpec with Matchers {
+
+  import fos.{ SimplyTyped, Term, True, False, Zero, If, Succ, Pred, IsZero, Var, Abs, App, Pair, First, Second, Bool, Nat, Function, Product }
+  import fos.test.helpers.Helper._
+
+}
