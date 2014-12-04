506f3d2e41926b3657bf986ef0c454b8ccd9bde9
Marco Antognini <antognini.marco@gmail.com>
2014-12-04 09:15:41 +0100

    fix eclipse project, format code and fix compilation error with ???

--- 06 featherweight java/src/main/scala/fos/FJ.scala
+++ 06 featherweight java/src/main/scala/fos/FJ.scala
@@ -5,48 +5,49 @@ import scala.util.parsing.input._
 
 import scala.collection.mutable.HashMap
 
-/** This object implements a parser for Featherweight Java
+/**
+ * This object implements a parser for Featherweight Java
  *  check <a href="http://citeseer.ist.psu.edu/igarashi99featherweight.html">
  *  the paper</a>.
  */
-object FJ extends StandardTokenParsers   {
+object FJ extends StandardTokenParsers {
 
   lexical.delimiters ++= List(".", "{", "}", ",", "=", ";", "(", ")")
-  lexical.reserved   ++= List("class", "extends", "super", "return", "new")
+  lexical.reserved ++= List("class", "extends", "super", "return", "new")
 
-
-  /** <pre>
+  /**
+   * <pre>
    *  Program     ::= { ClassDef } Expr
    * </pre>
    */
   def Prog: Parser[Tree] = positioned(
-      rep(ClsDef) ~ Expr                          ^^ {case classes ~ main => Program(classes,main)}
-  )
+    rep(ClsDef) ~ Expr ^^ { case classes ~ main => Program(classes, main) })
 
-  /** <pre>
+  /**
+   * <pre>
    *  ClassDef ::= "class" C extends C "{"
    *                   { FieldDef } ConstructorDef  { MethodDef }
    *               "}"
    *  </pre>
    */
   def ClsDef: Parser[ClassDef] = positioned(
-      "class" ~ ident ~ "extends" ~ ident ~ "{" ~
-        rep(FldDef) ~ CtorDef ~ rep(MethDef) ~
+    "class" ~ ident ~ "extends" ~ ident ~ "{" ~
+      rep(FldDef) ~ CtorDef ~ rep(MethDef) ~
       "}"
-         ^^ { case "class" ~ name ~ "extends" ~ superc ~ "{" ~ fields ~ ctor ~ methods ~ "}"=> ClassDef(name,superc,fields,ctor,methods)}
-    | failure("illegal start of class definition")
-  )
+      ^^ { case "class" ~ name ~ "extends" ~ superc ~ "{" ~ fields ~ ctor ~ methods ~ "}" => ClassDef(name, superc, fields, ctor, methods) }
+      | failure("illegal start of class definition"))
 
-  /** <pre>
+  /**
+   * <pre>
    *  FieldDef       ::= ident ident ";"
    *  </pre>
    */
   def FldDef: Parser[FieldDef] = positioned(
-	ident ~ ident <~ ";"                       ^^ { case tpe ~ name => FieldDef(tpe,name)}
-    | failure("illegal start of field definition")
-  )
+    ident ~ ident <~ ";" ^^ { case tpe ~ name => FieldDef(tpe, name) }
+      | failure("illegal start of field definition"))
 
-  /** <pre>
+  /**
+   * <pre>
    *  ConstructorDef ::= ident "(" ArgList ")" "{"
    *                       "super" "(" Arguments ")" ";"
    *                       { ident "." ident "=" ident }
@@ -54,70 +55,67 @@ object FJ extends StandardTokenParsers   {
    *  </pre>
    */
   def CtorDef: Parser[CtrDef] = positioned(
-      ident ~ "(" ~ ParamList ~ ")" ~ "{" ~
-        "super" ~ "(" ~ VarList ~ ")" ~ ";" ~
-        rep(Init) ~
-      "}"  ^^ {case name ~ "(" ~ params ~ ")" ~ "{" ~ "super" ~ "(" ~ supArgs ~ ")" ~ ";" ~ seq ~ "}" => CtrDef(name,params,supArgs,seq)}
-    | failure("illegal start of constructor")
-  )
+    ident ~ "(" ~ ParamList ~ ")" ~ "{" ~
+      "super" ~ "(" ~ VarList ~ ")" ~ ";" ~
+      rep(Init) ~
+      "}" ^^ { case name ~ "(" ~ params ~ ")" ~ "{" ~ "super" ~ "(" ~ supArgs ~ ")" ~ ";" ~ seq ~ "}" => CtrDef(name, params, supArgs, seq) }
+      | failure("illegal start of constructor"))
 
   /**
    * Initializer ::= ident "." ident "=" ident ";"
    */
   def Init: Parser[Assign] = positioned(
-      ident ~ "." ~ ident ~ "=" ~ ident <~ ";" ^^ {case obj ~ "." ~ field ~ "=" ~ value => Assign(obj, field, Var(value))}
-  )
+    ident ~ "." ~ ident ~ "=" ~ ident <~ ";" ^^ { case obj ~ "." ~ field ~ "=" ~ value => Assign(obj, field, Var(value)) })
 
-  /** <pre>
+  /**
+   * <pre>
    *  ParamList ::= ident ident { "," ident ident }
    *            | empty
    *  </pre>
    */
   def ParamList: Parser[List[FieldDef]] = (
-        ident ~ ident ~ rep("," ~> ident ~ ident  ^^ {case tpe ~ name => FieldDef(tpe,name)}) ^^ {case tpe ~ name ~ rest => FieldDef(tpe,name) :: rest}
-      | success(Nil: List[FieldDef])
-  )
+    ident ~ ident ~ rep("," ~> ident ~ ident ^^ { case tpe ~ name => FieldDef(tpe, name) }) ^^ { case tpe ~ name ~ rest => FieldDef(tpe, name) :: rest }
+    | success(Nil: List[FieldDef]))
 
-  /** <pre>
+  /**
+   * <pre>
    *  VarList ::= ident ident { "," ident ident }
    *            | epsilon
    *  </pre>
    */
   def VarList: Parser[List[Var]] = (
-        ident ~ rep("," ~> ident ^^ {x => Var(x)})   ^^  { case name ~ rest => Var(name) :: rest }
-      | success(Nil: List[Var])
-  )
+    ident ~ rep("," ~> ident ^^ { x => Var(x) }) ^^ { case name ~ rest => Var(name) :: rest }
+    | success(Nil: List[Var]))
 
-  /** <pre>
+  /**
+   * <pre>
    *  MethodDef ::= ident ident "(" Params ")" "{" "return" Expr ";" "}"
    *  </pre>
    */
   def MethDef: Parser[MethodDef] = positioned(
-        ident ~ ident ~ "(" ~ ParamList ~ ")" ~ "{" ~ "return" ~ Expr ~ ";" ~ "}"
-          ^^  {case tpe ~ name ~ "(" ~ params ~ ")" ~ "{" ~ "return" ~ body ~ ";" ~ "}" => MethodDef(tpe,name,params,body)}
-      | failure("illegal start of method definition")
-  )
+    ident ~ ident ~ "(" ~ ParamList ~ ")" ~ "{" ~ "return" ~ Expr ~ ";" ~ "}"
+      ^^ { case tpe ~ name ~ "(" ~ params ~ ")" ~ "{" ~ "return" ~ body ~ ";" ~ "}" => MethodDef(tpe, name, params, body) }
+      | failure("illegal start of method definition"))
 
-
-  /** <pre>
+  /**
+   * <pre>
    *  Expressions ::= [Expr { "," Expr } ]
    *  </pre>
    */
   def Expressions: Parser[List[Expr]] = (
-        Expr ~ rep("," ~> Expr)  ^^ {case expr ~ rest => expr :: rest }
-      | success(Nil: List[Expr])
-  )
+    Expr ~ rep("," ~> Expr) ^^ { case expr ~ rest => expr :: rest }
+    | success(Nil: List[Expr]))
 
-
-  /** <pre>
+  /**
+   * <pre>
    *  Expr ::= SimpleExpr {"." ident ["(" Expressions ")"]}
    *  </pre>
    */
   def Expr: Parser[Expr] = positioned(
-        SimpleExpr ~ rep("." ~> ident ~ opt("(" ~> Expressions <~ ")")) ^^ {case se ~ xs => mkExpression(se,xs)}
-  )
+    SimpleExpr ~ rep("." ~> ident ~ opt("(" ~> Expressions <~ ")")) ^^ { case se ~ xs => mkExpression(se, xs) })
 
-  /** <pre>
+  /**
+   * <pre>
    *  SimpleExpr ::= ident
    *               | "new" C "(" Expressions ")"
    *               | "(" C ")" Expr
@@ -125,12 +123,11 @@ object FJ extends StandardTokenParsers   {
    *  </pre>
    */
   def SimpleExpr: Parser[Expr] = positioned(
-        ident                                        ^^ {x => Var(x)}
-      | "new" ~ ident ~ "(" ~ Expressions ~ ")"      ^^ {case "new" ~ name ~ "(" ~ args ~ ")" => New(name,args)}
-      | "(" ~ ident ~ ")" ~ Expr                    ^^ {case "(" ~ tpe ~ ")" ~ obj => Cast(tpe,obj)}
+    ident ^^ { x => Var(x) }
+      | "new" ~ ident ~ "(" ~ Expressions ~ ")" ^^ { case "new" ~ name ~ "(" ~ args ~ ")" => New(name, args) }
+      | "(" ~ ident ~ ")" ~ Expr ^^ { case "(" ~ tpe ~ ")" ~ obj => Cast(tpe, obj) }
       | "(" ~> Expr <~ ")"
-      | failure("illegal start of simple expression")
-      )
+      | failure("illegal start of simple expression"))
 
   type Output = Tree
 
@@ -152,31 +149,31 @@ object FJ extends StandardTokenParsers   {
   import Type._
 
   def eval(t: Tree): Tree = {
-//    PrettyPrinter(t)
+    //    PrettyPrinter(t)
     t match {
       case Program(cls, expr) =>
         try {
-         cls foreach (cl => typeOf(cl, Nil))
-         val typeExpr = typeOf(expr,Nil)
-         println("TYPE EXPR: "+typeExpr);expr
-         val evExpr  = Evaluate(expr)
-         print("EVALUATE TO: ")
-         evExpr
+          cls foreach (cl => typeOf(cl, Nil))
+          val typeExpr = typeOf(expr, Nil)
+          println("TYPE EXPR: " + typeExpr); expr
+          val evExpr = Evaluate(expr)
+          print("EVALUATE TO: ")
+          evExpr
         } catch {
-        case TypeError(msg) =>
-          println("Type Error: "+msg)
-          print("The expression will not be evaluated. Expr: ")
-          CT.clear
-          expr
-        case EvaluationException(msg) =>
-          println("The expression generate an exception in Java: "+msg)
-          CT.clear
-          expr
+          case TypeError(msg) =>
+            println("Type Error: " + msg)
+            print("The expression will not be evaluated. Expr: ")
+            CT.clear
+            expr
+          case EvaluationException(msg) =>
+            println("The expression generate an exception in Java: " + msg)
+            CT.clear
+            expr
 
-        case e @ _ =>
-          println(e)
-          CT.clear
-          expr
+          case e @ _ =>
+            println(e)
+            CT.clear
+            expr
         }
       case _ =>
         println("The file must start with a class definition")
--- 06 featherweight java/src/main/scala/fos/Tree.scala
+++ 06 featherweight java/src/main/scala/fos/Tree.scala
@@ -1,10 +1,11 @@
+
 package fos
 
 import scala.util.parsing.combinator.syntactical.StandardTokenParsers
 import scala.util.parsing.input._
 
 import java.io.PrintWriter
-import scala.collection.immutable.{Map,ListMap}
+import scala.collection.immutable.{ Map, ListMap }
 
 import CT._
 import Utils._
@@ -22,11 +23,10 @@ case class MethodArgsLengthException(msg: String) extends MethodException(msg)
 abstract class FieldException(msg: String) extends TreeException(msg)
 case class FieldAlreadyDefined(msg: String) extends FieldException(msg)
 
-
 sealed abstract class Tree extends Positional
 
 case class Program(cls: List[ClassDef], expr: Expr) extends Tree {
-  cls.foreach(c => CT.add(c.name,c));
+  cls.foreach(c => CT.add(c.name, c));
 }
 case class ClassDef(name: String, superclass: String, fields: List[FieldDef], ctor: CtrDef, methods: List[MethodDef]) extends Tree {
   private def fieldLookup: List[FieldDef] = {
@@ -49,7 +49,7 @@ case class ClassDef(name: String, superclass: String, fields: List[FieldDef], ct
   private def checkListFieldsDef(f: List[FieldDef]): Unit = {
     f find (field => (f count (_field => _field.name == field.name)) > 1) match {
       case None => ()
-      case Some(varfield) => throw FieldAlreadyDefined("variable "+varfield.name+" is already defined in the scope")
+      case Some(varfield) => throw FieldAlreadyDefined("variable " + varfield.name + " is already defined in the scope")
     }
   }
 
@@ -62,50 +62,50 @@ case class ClassDef(name: String, superclass: String, fields: List[FieldDef], ct
   }
 
   def overrideMethod(tpe: String, name: String, args: List[FieldDef], body: Expr): Unit = {
-    if( (methods count (m => m.name == name)) > 1) throw new MethodOverrideException(", method "+name+" is defined more than once")
+    if ((methods count (m => m.name == name)) > 1) throw new MethodOverrideException(", method " + name + " is defined more than once")
     try {
       checkListFieldsDef(args);
     } catch {
-      case FieldAlreadyDefined(msg) => throw FieldAlreadyDefined("In class "+this.name+", in the arguments of method "+name+", "+msg)
+      case FieldAlreadyDefined(msg) => throw FieldAlreadyDefined("In class " + this.name + ", in the arguments of method " + name + ", " + msg)
     }
-    val inheritedMethod = getClassDef(superclass) findMethod(name);
+    val inheritedMethod = getClassDef(superclass) findMethod (name);
     inheritedMethod match {
       case None => ()
-      case Some(MethodDef(tpeS,_,argsS, _)) =>
+      case Some(MethodDef(tpeS, _, argsS, _)) =>
         var error = false;
-        if(tpe == tpeS) {
+        if (tpe == tpeS) {
           val paramsOvMethod = args map (arg => arg.tpe);
           val paramsInMethod = argsS map (argS => argS.tpe);
-          if(args.length != argsS.length)
-            throw new MethodOverrideException("can't apply "+paramsOvMethod.mkString ("(",",",")")+" to "+paramsInMethod.mkString ("(",",",")"))
-          for (i <- List.range(0,paramsInMethod length)) {
-            if( (paramsInMethod(i) != paramsOvMethod(i)) && !error) error = true;
+          if (args.length != argsS.length)
+            throw new MethodOverrideException("can't apply " + paramsOvMethod.mkString("(", ",", ")") + " to " + paramsInMethod.mkString("(", ",", ")"))
+          for (i <- List.range(0, paramsInMethod length)) {
+            if ((paramsInMethod(i) != paramsOvMethod(i)) && !error) error = true;
           }
-          if(error)
-            throw new MethodOverrideException("can't apply "+paramsOvMethod .mkString ("(",",",")")+" to "+paramsInMethod.mkString ("(",",",")"))
+          if (error)
+            throw new MethodOverrideException("can't apply " + paramsOvMethod.mkString("(", ",", ")") + " to " + paramsInMethod.mkString("(", ",", ")"))
           () //Everything was ok, so override ok
         } else
-          throw new MethodOverrideException("Type mismatch. The return type "+tpeS+" of inherithed "+
-            "method "+name+" has different signature. Overriding method has type "+tpe)
+          throw new MethodOverrideException("Type mismatch. The return type " + tpeS + " of inherithed " +
+            "method " + name + " has different signature. Overriding method has type " + tpe)
     }
   }
- /**
-  * checkTypeArguments: verify only the type of the parameters not the name
-  * Should throw ClassConstructorArgsException's.
-  */
-  def checkTypeArguments(argsType: List[String]): Unit =  {
-    var errorSub = Pair(false,0);
+  /**
+   * checkTypeArguments: verify only the type of the parameters not the name
+   * Should throw ClassConstructorArgsException's.
+   */
+  def checkTypeArguments(argsType: List[String]): Unit = {
+    var errorSub = Pair(false, 0);
     val typeFields: List[String] = fieldLookup map (fd => fd.tpe);
-    if( (typeFields length) == (argsType length)) {
-      for (i <- List.range(0,typeFields length)) {
-        if( !( getClassDef(argsType(i)).isSubClassOf(typeFields(i)) ) && !errorSub._1) errorSub = Pair(true,i);
+    if ((typeFields length) == (argsType length)) {
+      for (i <- List.range(0, typeFields length)) {
+        if (!(getClassDef(argsType(i)).isSubClassOf(typeFields(i))) && !errorSub._1) errorSub = Pair(true, i);
       }
-      if(errorSub._1)
-        throw new ClassConstructorArgsException("can't apply "+argsType .mkString ("(",",",")")+" to "+typeFields.mkString ("(",",",")")+" because "+
-          argsType(errorSub._2)+" is not a subtype of "+typeFields(errorSub._2))
+      if (errorSub._1)
+        throw new ClassConstructorArgsException("can't apply " + argsType.mkString("(", ",", ")") + " to " + typeFields.mkString("(", ",", ")") + " because " +
+          argsType(errorSub._2) + " is not a subtype of " + typeFields(errorSub._2))
       () //no errors means everything was fine
     } else
-      throw new ClassConstructorArgsException("can't apply "+argsType .mkString ("(",",",")")+" to "+typeFields .mkString ("(",",",")"))
+      throw new ClassConstructorArgsException("can't apply " + argsType.mkString("(", ",", ")") + " to " + typeFields.mkString("(", ",", ")"))
   }
 
   /**
@@ -113,47 +113,46 @@ case class ClassDef(name: String, superclass: String, fields: List[FieldDef], ct
    * Should throw FieldAlreadyDefined and ClassConstructorArgsException.
    */
   def verifyConstructorArgs: Unit = {
-     try {
-       checkListFieldsDef (ctor.args);
-     } catch {
-       case FieldAlreadyDefined(msg) => throw FieldAlreadyDefined(", in the constructor, "+msg)
-     }
+    try {
+      checkListFieldsDef(ctor.args);
+    } catch {
+      case FieldAlreadyDefined(msg) => throw FieldAlreadyDefined(", in the constructor, " + msg)
+    }
     val fieldss = fieldLookup;
     val fieldsType = fieldss map (fd => fd.tpe);
     val constructType = ctor.args map (arg => arg.tpe);
-    if (fieldss.length != (ctor.args length) )
-      throw new ClassConstructorArgsException("can't apply "+constructType.mkString ("(",",",")")+
-        " to "+fieldsType.mkString ("(",",",")"))
-    (fieldss zip ctor.args) foreach ( pair => {
-      if(pair._1 != pair._2)
-        throw new ClassConstructorArgsException("can't apply "+(ctor.args).mkString ("(",",",")")+
-          " to "+fieldss.mkString ("(",",",")"))
+    if (fieldss.length != (ctor.args length))
+      throw new ClassConstructorArgsException("can't apply " + constructType.mkString("(", ",", ")") +
+        " to " + fieldsType.mkString("(", ",", ")"))
+    (fieldss zip ctor.args) foreach (pair => {
+      if (pair._1 != pair._2)
+        throw new ClassConstructorArgsException("can't apply " + (ctor.args).mkString("(", ",", ")") +
+          " to " + fieldss.mkString("(", ",", ")"))
     })
     () //No error means every parameter of the constructor has the same type and name of the ones in the class
   }
-
 
   def superClass: Option[ClassDef] = CT lookup (this superclass)
 
   def isSuperclassOf(that: Option[ClassDef]): Boolean =
     that match {
-    case None => false
-    case Some(sub) =>
-      //C <: C
-      if(name == sub.name) true
-      else {
-        // CT(C) = class C extends D {...} -> C <: D OR C <: D & D <: E -> C <: E
-        this isSuperclassOf (sub superClass)
+      case None => false
+      case Some(sub) =>
+        //C <: C
+        if (name == sub.name) true
+        else {
+          // CT(C) = class C extends D {...} -> C <: D OR C <: D & D <: E -> C <: E
+          this isSuperclassOf (sub superClass)
+        }
     }
-  }
 
   def isSubClassOf(that: ClassDef): Boolean = that isSuperclassOf Some(this)
 
-  def isSubClassOf(that: String) : Boolean = isSubClassOf (getClassDef(that))
+  def isSubClassOf(that: String): Boolean = isSubClassOf(getClassDef(that))
 }
 
 case class FieldDef(tpe: String, name: String) extends Tree {
-  override def toString = tpe+" "+name
+  override def toString = tpe + " " + name
 }
 
 case class CtrDef(name: String, args: List[FieldDef], supers: List[Var], body: List[Assign]) extends Tree
@@ -165,18 +164,18 @@ case class MethodDef(tpe: String, name: String, args: List[FieldDef], body: Expr
   /*
    * Check type arguments of method definiton. Should throw MethodArgsException's.
    */
-  def checkTypeArguments(argsType: List[String]): Unit =  {
-      var error = false;
-      val params = (args map (arg => arg.tpe));
-      if( (params length) == (argsType length)) {
-        for (i <- List.range(0,params length)) {
-          if( !((getClassDef(argsType(i))) isSubClassOf params(i)) && !error) error = true;
-        }
-        if(error)
-          throw new MethodArgsException("can't apply "+argsType.mkString ("(",",",")")+" to "+params.mkString ("(",",",")"))
-        ()
-      } else
-        throw new MethodArgsException("can't apply "+argsType.mkString ("(",",",")")+" to "+params.mkString ("(",",",")"))
+  def checkTypeArguments(argsType: List[String]): Unit = {
+    var error = false;
+    val params = (args map (arg => arg.tpe));
+    if ((params length) == (argsType length)) {
+      for (i <- List.range(0, params length)) {
+        if (!((getClassDef(argsType(i))) isSubClassOf params(i)) && !error) error = true;
+      }
+      if (error)
+        throw new MethodArgsException("can't apply " + argsType.mkString("(", ",", ")") + " to " + params.mkString("(", ",", ")"))
+      ()
+    } else
+      throw new MethodArgsException("can't apply " + argsType.mkString("(", ",", ")") + " to " + params.mkString("(", ",", ")"))
   }
 }
 
@@ -186,18 +185,17 @@ case class Var(name: String) extends Expr {
   override def toString = name
 }
 case class New(cls: String, args: List[Expr]) extends Expr {
-  override def toString = "new "+cls+""+args.mkString("(",",",")")
+  override def toString = "new " + cls + "" + args.mkString("(", ",", ")")
 }
 case class Cast(cls: String, e: Expr) extends Expr {
-  override def toString = "( ("+cls+")"+e+")"
+  override def toString = "( (" + cls + ")" + e + ")"
 }
 case class Select(obj: Expr, field: String) extends Expr {
-  override def toString = obj+"."+field
+  override def toString = obj + "." + field
 }
 case class Apply(obj: Expr, method: String, args: List[Expr]) extends Expr {
-  override def toString = obj+"."+method+""+args.mkString("(",",",")")
+  override def toString = obj + "." + method + "" + args.mkString("(", ",", ")")
 }
-
 
 /**
  * Pretty printer using scala.text formatting library. It works
@@ -205,7 +203,7 @@ case class Apply(obj: Expr, method: String, args: List[Expr]) extends Expr {
  * new lines, and afterwards rendering the text given a Writer and a
  * desired width.
  */
-object PrettyPrinter  {
+object PrettyPrinter {
   def apply(t: Tree) = {
     val writer = new PrintWriter(System.out)
     toDocument(t).format(80, writer)
@@ -218,9 +216,10 @@ object PrettyPrinter  {
 
   def toDocument(ts: List[Tree], sep: String, suffix: String): Document = ts match {
     case one :: two :: rest =>
-      toDocument(one) :: sep :/: rest.foldLeft(toDocument(two))  { (d, e) =>
+      toDocument(one) :: sep :/: rest.foldLeft(toDocument(two)) { (d, e) =>
         if (sep != "") d :: sep :/: toDocument(e)
-        else d :/: toDocument(e) } :: text(suffix)
+        else d :/: toDocument(e)
+      } :: text(suffix)
     case one :: Nil =>
       toDocument(one) :: text(suffix)
     case Nil =>
@@ -233,19 +232,18 @@ object PrettyPrinter  {
 
     case ClassDef(name, superclass, fields, ctor, methods) =>
       group("class " :: name :/: "extends " :: superclass :: empty) ::
-         nest(2, " {" :/: group(toDocument(fields, ";", ";") :/: toDocument(ctor) :/: toDocument(methods, "", ""))) :/:
-      "}" :/: empty
+        nest(2, " {" :/: group(toDocument(fields, ";", ";") :/: toDocument(ctor) :/: toDocument(methods, "", ""))) :/:
+        "}" :/: empty
 
     case FieldDef(tpe, name) =>
       group(tpe :/: text(name))
 
     case CtrDef(name, args, supers, body) =>
       group(name :: "(" :: group(toDocument(args, ",", "")) :: ")" :: empty) :/:
-        nest(2,  "{" :/:
-            group("super(" :: group(toDocument(supers, ",", "")) :: ");" :/: empty) :/:
-            group(toDocument(body, "", ""))
-            ) :/:
-      "}" :/: empty
+        nest(2, "{" :/:
+          group("super(" :: group(toDocument(supers, ",", "")) :: ");" :/: empty) :/:
+          group(toDocument(body, "", ""))) :/:
+        "}" :/: empty
 
     case Assign(obj, field, rhs) =>
       group(obj :: "." :: field :/: "=" :/: toDocument(rhs) :: ";" :/: empty)
@@ -253,7 +251,7 @@ object PrettyPrinter  {
     case MethodDef(tpe, name, args, body) =>
       group(tpe :/: name :/: "(" :: group(toDocument(args, ",", "")) :: ")" :/: text("{")) :/:
         nest(2, "return " :: group(toDocument(body))) :/:
-      "}" :/: empty
+        "}" :/: empty
 
     case Var(name) =>
       text(name)
